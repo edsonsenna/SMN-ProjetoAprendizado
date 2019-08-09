c03a656e96c4ad7528b307c7bd6035db2b553b55
Edson Jr <edsonsjr14@outlook.com>
2019-08-09 11:09:12 -0300

    Correcoes

--- ProjetoAprendizado/BNK.Web.Application/Contas/Model/ContaModel.cs
+++ ProjetoAprendizado/BNK.Web.Application/Contas/Model/ContaModel.cs
@@ -9,5 +9,6 @@ namespace BNK.Web.Application.Contas.Model
         public int Cod_TipoConta { get; set; }
         public DateTime Date_DataCriacao { get; set; }
         public int Num_SeqlUsuar { get; set; }
+        public string Nom_NomeUsuar { get; set; }
     }
 }
